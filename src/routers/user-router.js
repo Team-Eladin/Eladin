7323a1aed2bb232c5a53f7c8b7fa53931535e7a9
wldn143 <wldn143@gmail.com>
2022-05-25 10:51:36 +0900

    test user api 삭제

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -92,17 +92,7 @@ userRouter.get("/userlist", loginRequired, async function (req, res, next) {
     next(error);
   }
 });
-userRouter.get("/user", async function (req, res, next) {
-  try {
-    // 전체 사용자 목록을 얻음
-    const users = await userService.getUsers();
 
-    // 사용자 목록(배열)을 JSON 형태로 프론트에 보냄
-    res.status(200).json(users);
-  } catch (error) {
-    next(error);
-  }
-});
 // 사용자 정보 수정
 // (예를 들어 /api/users/abc12345 로 요청하면 req.params.userId는 'abc12345' 문자열로 됨)
 userRouter.patch(
