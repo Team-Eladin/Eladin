788c4cfd828090327d2fc88ff9571a650109403e
wldn143 <wldn143@gmail.com>
2022-05-25 10:46:51 +0900

    사용자 정보 삭제, 회원가입 유효성 검사

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,49 +1,67 @@
-import { Router } from 'express';
-import is from '@sindresorhus/is';
+import { Router } from "express";
+import { model } from "mongoose";
+import is from "@sindresorhus/is";
 // 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
-import { loginRequired } from '../middlewares';
-import { userService } from '../services';
-
+import { loginRequired } from "../middlewares";
+import { userService } from "../services";
+import { UserSchema } from "../db/schemas/user-schema";
+const { body, validationResult } = require("express-validator");
+const { validateError } = require("../middlewares/validate");
 const userRouter = Router();
-
+const User = model("users", UserSchema);
 // 회원가입 api (아래는 /register이지만, 실제로는 /api/register로 요청해야 함.)
-userRouter.post('/register', async (req, res, next) => {
-  try {
-    // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
-    // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        'headers의 Content-Type을 application/json으로 설정해주세요'
-      );
+userRouter.post(
+  "/register",
+  [
+    body("fullName")
+      .trim()
+      .isLength({ min: 2 })
+      .withMessage("2글자 이상의 이름을 입력해주세요."),
+    body("email").isEmail().withMessage("이메일 형식이 맞지 않습니다."),
+    body("password")
+      .trim()
+      .isLength({ min: 4 })
+      .withMessage("4자리 이상의 비밀번호를 입력해주세요."),
+    validateError,
+  ],
+  async (req, res, next) => {
+    try {
+      // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
+      // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+      if (is.emptyObject(req.body)) {
+        throw new Error(
+          "headers의 Content-Type을 application/json으로 설정해주세요"
+        );
+      }
+
+      // req (request)의 body 에서 데이터 가져오기
+      const fullName = req.body.fullName;
+      const email = req.body.email;
+      const password = req.body.password;
+
+      // 위 데이터를 유저 db에 추가하기
+      const newUser = await userService.addUser({
+        fullName,
+        email,
+        password,
+      });
+
+      // 추가된 유저의 db 데이터를 프론트에 다시 보내줌
+      // 물론 프론트에서 안 쓸 수도 있지만, 편의상 일단 보내 줌
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
     }
-
-    // req (request)의 body 에서 데이터 가져오기
-    const fullName = req.body.fullName;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    // 위 데이터를 유저 db에 추가하기
-    const newUser = await userService.addUser({
-      fullName,
-      email,
-      password,
-    });
-
-    // 추가된 유저의 db 데이터를 프론트에 다시 보내줌
-    // 물론 프론트에서 안 쓸 수도 있지만, 편의상 일단 보내 줌
-    res.status(201).json(newUser);
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 // 로그인 api (아래는 /login 이지만, 실제로는 /api/login로 요청해야 함.)
-userRouter.post('/login', async function (req, res, next) {
+userRouter.post("/login", async function (req, res, next) {
   try {
     // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
     if (is.emptyObject(req.body)) {
       throw new Error(
-        'headers의 Content-Type을 application/json으로 설정해주세요'
+        "headers의 Content-Type을 application/json으로 설정해주세요"
       );
     }
 
@@ -63,7 +81,7 @@ userRouter.post('/login', async function (req, res, next) {
 
 // 전체 유저 목록을 가져옴 (배열 형태임)
 // 미들웨어로 loginRequired 를 썼음 (이로써, jwt 토큰이 없으면 사용 불가한 라우팅이 됨)
-userRouter.get('/userlist', loginRequired, async function (req, res, next) {
+userRouter.get("/userlist", loginRequired, async function (req, res, next) {
   try {
     // 전체 사용자 목록을 얻음
     const users = await userService.getUsers();
@@ -74,11 +92,21 @@ userRouter.get('/userlist', loginRequired, async function (req, res, next) {
     next(error);
   }
 });
+userRouter.get("/user", async function (req, res, next) {
+  try {
+    // 전체 사용자 목록을 얻음
+    const users = await userService.getUsers();
 
+    // 사용자 목록(배열)을 JSON 형태로 프론트에 보냄
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+});
 // 사용자 정보 수정
 // (예를 들어 /api/users/abc12345 로 요청하면 req.params.userId는 'abc12345' 문자열로 됨)
 userRouter.patch(
-  '/users/:userId',
+  "/users/:userId",
   loginRequired,
   async function (req, res, next) {
     try {
@@ -86,7 +114,7 @@ userRouter.patch(
       // 설정 안 하고 요청하면, body가 비어 있게 됨.
       if (is.emptyObject(req.body)) {
         throw new Error(
-          'headers의 Content-Type을 application/json으로 설정해주세요'
+          "headers의 Content-Type을 application/json으로 설정해주세요"
         );
       }
 
@@ -105,7 +133,7 @@ userRouter.patch(
 
       // currentPassword 없을 시, 진행 불가
       if (!currentPassword) {
-        throw new Error('정보를 변경하려면, 현재의 비밀번호가 필요합니다.');
+        throw new Error("정보를 변경하려면, 현재의 비밀번호가 필요합니다.");
       }
 
       const userInfoRequired = { userId, currentPassword };
@@ -128,6 +156,29 @@ userRouter.patch(
 
       // 업데이트 이후의 유저 데이터를 프론트에 보내 줌
       res.status(200).json(updatedUserInfo);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//계정 삭제
+userRouter.delete(
+  "/users/:userId",
+  loginRequired,
+  async function (req, res, next) {
+    try {
+      const userId = req.params.userId;
+      User.findOne({
+        where: {
+          _id: userId,
+        },
+      }).then((result) => {
+        result.deleteOne({
+          _id: userId,
+        });
+        res.send(result);
+      });
     } catch (error) {
       next(error);
     }
